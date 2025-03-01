1dab772029f1f2859d409379293e3d7ef31b0875
noeljstesalit <noeljabraham@stesalitsystems.com>
2025-03-01 17:52:52 +0530

    until prob 6 rename

--- loops/problem2.py
+++ loops/problem2.py
@@ -5,7 +5,7 @@ sum_even=0
 
 for i in range(1,n+1):
     if i%2 == 0:
-        sum_even+=1
+        sum_even+=i
 print(sum_even)
 
 
--- loops/problem6.py
+++ loops/problem6.py
@@ -1,2 +1,9 @@
 # todo factorial of number
 
+number = 5
+factorial = 1
+
+while number > 0:
+    factorial = factorial * number
+    number = number - 1
+print(factorial)
